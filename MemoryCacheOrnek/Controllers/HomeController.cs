8c26e2c6837a3e5694183e71e041408dbac9a055
CanSinan <pmsinancan@gmail.com>
2023-11-11 22:51:15 +0300

    Basit bir şekilde MemoryCache işlemi yaptık. Bu işlemimizi projeyi çalıştırıp browserımızda kontrol ettik.

--- MemoryCacheOrnek/Controllers/HomeController.cs
+++ MemoryCacheOrnek/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using MemoryCacheOrnek.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using System.Diagnostics;
 
 namespace MemoryCacheOrnek.Controllers
@@ -7,14 +8,39 @@ namespace MemoryCacheOrnek.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IMemoryCache _memoryCache;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IMemoryCache memoryCache)
         {
             _logger = logger;
+            _memoryCache = memoryCache;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var chachedValue = await _memoryCache.GetOrCreateAsync<string>("product", cacheEntry =>
+            {
+                cacheEntry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(20);
+                return Task.FromResult($"DateTime:{DateTime.Now}");
+            });
+            ViewBag.CacheValue = chachedValue;
+
+            var productList = new List<Product>();
+            var productsCached = await _memoryCache.GetOrCreateAsync<List<Product>>("productList", cacheEntr =>
+            {
+                cacheEntr.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(20);
+                for (int i = 0; i < 1000; i++)
+                {
+                    productList.Add(new Product
+                    {
+                        Name = "Product -" + (i + 1).ToString(),
+                        Id = i,
+                        CreatedDate = DateTime.Now,
+                    });
+                }
+                return Task.FromResult(productList);
+            });
+            ViewBag.ProductList = productsCached;
             return View();
         }
 
